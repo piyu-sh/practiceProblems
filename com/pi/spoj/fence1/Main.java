a6c9963484fac9b1cbe3cb20b71102b6f86eab80
pi <killerpgupta007@gmail.com>
2016-04-25 00:15:05 +0530

    spoj practice

--- com/pi/spoj/fence1/Main.java
+++ com/pi/spoj/fence1/Main.java
@@ -1,6 +1,8 @@
+package com.pi.spoj.fence1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.DecimalFormat;
 
 public class Main {
 
@@ -11,7 +13,8 @@ public class Main {
 		while (!"0".equals((num = br.readLine().trim()))) {
 
 			double result = Math.pow(Integer.parseInt(num), 2) / (2 * Math.PI);
-			double finalValue = Math.round(result * 100.0) / 100.0;
+			DecimalFormat newFormat = new DecimalFormat("0.00");
+			String finalValue = newFormat.format(result);
 			System.out.println(finalValue);
 		}
 	}
--- com/pi/spoj/tricount/Main.java
+++ com/pi/spoj/tricount/Main.java
@@ -2,22 +2,15 @@ package com.pi.spoj.tricount;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 public class Main {
-
 	public static void main(String[] args) throws NumberFormatException,
 			IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int testCases = Integer.parseInt(br.readLine().trim());
-		for (int i = 0; i < testCases; i++) {
-
-			int N = Integer.parseInt(br.readLine().trim());
-
-			long result = (long) (Math.pow(2, N + 1) - 1) - 2;
-			System.out.println(result);
-
+		int tC = Integer.parseInt(br.readLine().trim());
+		for (int i = 0; i < tC; i++) {
+			long N = Long.parseLong(br.readLine().trim());
+			long j = (N*(N+2)*(2*N+1))/8;
+			System.out.println(j);
 		}
 	}
-
 }
