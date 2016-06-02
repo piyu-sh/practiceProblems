a27c8162a5f7d256043e2c909bc4e6dcf9dcd231
pi <killerpgupta007@gmail.com>
2016-06-02 23:21:15 +0530

    spoj practice

--- Main.java
+++ Main.java
@@ -1,80 +1,83 @@
+import static java.lang.Integer.parseInt;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException,
-			IOException {
+	// public static void main(String[] args) throws NumberFormatException,
+	// IOException {
+	//
+	// Scanner scan = new Scanner(System.in);
+	// int test = scan.nextInt();
+	// for (int i = 0; i < test; i++) {
+	// int h = scan.nextInt();
+	// int w = scan.nextInt();
+	// int[][] input = new int[h][w];
+	// for (int j = 0; j < h; j++) {
+	// for (int j2 = 0; j2 < w; j2++) {
+	// input[j][j2] = scan.nextInt();
+	// }
+	// }
+	//
+	// for (int j = h - 2; j >= 0; j--) {
+	// for (int j2 = 0; j2 < w; j2++) {
+	// input[j][j2] += Math.max(
+	// j2 == 0 ? 0 : input[j + 1][j2 - 1], Math.max(
+	// input[j + 1][j2], j2 == w - 1 ? 0
+	// : input[j + 1][j2 + 1]));
+	// }
+	// }
+	// int max = input[0][0];
+	// for (int j = 0; j < w; j++) {
+	// max = max > input[0][j] ? max : input[0][j];
+	// }
+	// System.out.println(max);
+	// }
+	// scan.close();
+	// }
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int testCases = Integer.parseInt(br.readLine().trim());
-		for (int i = 0; i < testCases; i++) {
-			String split[] = br.readLine().trim().split(" ");
-			int h = Integer.parseInt(split[0]);
-			int w = Integer.parseInt(split[1]);
-			int[][] input = new int[h][w];
-			for (int j = 0; j < h; j++) {
-				split = br.readLine().trim().split(" ");
-				for (int j2 = 0; j2 < w; j2++) {
-					input[j][j2] = Integer.parseInt(split[j2]);
+	public static void main(String[] args) throws IOException {
+		BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
+		int T = parseInt(f.readLine().trim());
+		int w, h, i, j, max;
+		StringTokenizer st;
+		int[][] grid;
+		while (T-- > 0) {
+			st = new StringTokenizer(f.readLine());
+			w = parseInt(st.nextToken().trim());
+			h = parseInt(st.nextToken().trim());
+			grid = new int[w][h + 2];
+			for (i = 0; i < w; i++) {
+				for (j = 1; j <= h; j++) {
+					if (!st.hasMoreTokens())
+						st = new StringTokenizer(f.readLine());
+					grid[i][j] = parseInt(st.nextToken().trim());
 				}
+				grid[i][0] = grid[i][h + 1] = -100000000;
 			}
-			int[][][] output = new int[h][w][2];
-			for (int j = 0; j < h; j++) {
-				for (int j2 = 0; j2 < w; j2++) {
-					if (j == 0) {
-						output[j][j2][0] = j2;
-						output[j][j2][1] = input[j][j2];
-					} else {
-						int wOfMax = 0;
-						int wLeft = j2 == 0 ? -1 : j2 - 1;
-						int wCenter = j2;
-						int wRight = j2 == w - 1 ? -1 : j2 + 1;
-						if (wLeft != -1) {
-							if (wRight != -1) {
-								if (input[j][wLeft] > input[j][wCenter]) {
-									if (input[j][wLeft] > input[j][wRight]) {
-										wOfMax = wLeft;
-									} else {
-										wOfMax = wRight;
-									}
-								} else {
-									if (input[j][wCenter] > input[j][wRight]) {
-										wOfMax = wCenter;
-									} else {
-										wOfMax = wRight;
-									}
-								}
-							} else {
-								if (input[j][wLeft] > input[j][wCenter]) {
-									wOfMax = wLeft;
-								} else {
-									wOfMax = wCenter;
-								}
-							}
-						} else {
-							if (input[j][wCenter] > input[j][wRight]) {
-								wOfMax = wCenter;
-							} else {
-								wOfMax = wRight;
-							}
-						}
 
-						output[j][j2][0] = wOfMax;
-						output[j][j2][1] = output[j-1][j2][1]+input[j][wOfMax];
-					}
-				}
-			}
-			int maxStones = output[0][0][1];
-			for (int j = 0; j < w; j++) {
-				if (output[h - 1][j][1] > maxStones) {
-					maxStones = output[h - 1][j][1];
-				}
-			}
-			System.out.println(maxStones);
-			// instead take max for each cell
+			for (i = 1; i < w; i++)
+				for (j = 1; j <= h; j++)
+					grid[i][j] += max(grid[i - 1][j - 1], grid[i - 1][j],
+							grid[i - 1][j + 1]);
+
+			max = 0;
+			for (j = 1; j <= h; j++)
+				max = max(max, grid[w - 1][j]);
+
+			System.out.println(max);
+			System.out.flush();
 		}
 	}
 
+	private static int max(int... x) {
+		int max = -Integer.MAX_VALUE;
+		for (int i : x)
+			if (max < i)
+				max = i;
+		return max;
+	}
 }
