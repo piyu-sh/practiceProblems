1f10bb0fa55f5652c7e19daac60ea34975f6c5fc
pi <killerpgupta007@gmail.com>
2016-06-10 00:23:21 +0530

    spoj practice

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException,
+			IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String num = br.readLine().trim();
+		long result = 0;
+		Set<Long> unique = new HashSet<Long>();
+		long count = 0;
+
+		while (result != 1) {
+			unique.add(result);
+			num = result == 0 ? num : String.valueOf(result);
+			result = 0;
+			for (int i = 0; i < num.length(); i++) {
+				result += Math
+						.pow(Integer.parseInt(num.substring(i, i + 1)), 2);
+			}
+			if (unique.contains(result)) {
+				count = -1;
+				break;
+			}
+			count++;
+
+		}
+		System.out.println(count);
+	}
+
+}
