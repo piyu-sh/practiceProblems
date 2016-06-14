3fb5a4d448d3c9c034ea38102ce81331e507456d
pi <killerpgupta007@gmail.com>
2016-06-14 23:45:59 +0530

    spoj practice

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//break into power of two
+		//137=2(2(2)+2+2(0))+2(2+2(0))+2(0)
+		//substitute 2(2) with 2^2
+		System.out.println(getResult(137));
+	}
+
+	private static String getResult(int i) {
+		String result = null;
+		
+		
+		
+		return result;
+	}
+
+}
