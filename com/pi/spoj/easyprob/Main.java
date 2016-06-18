03c1af7d8591b40ed2dff6e0f7ddc35b693897ac
pi <killerpgupta007@gmail.com>
2016-06-18 15:59:17 +0530

    spoj practice

--- com/pi/spoj/easyprob/Main.java
+++ com/pi/spoj/easyprob/Main.java
@@ -6,56 +6,56 @@ public class Main {
 		// break into power of two
 		// 137=2(2(2)+2+2(0))+2(2+2(0))+2(0)
 		// substitute 2(2) with 2^2
+		System.out.print("137=");
 		getResult(137);
+		System.out.println();
+		System.out.print("1315=");
+		getResult(1315);
+		System.out.println();
+		System.out.print("73=");
+		getResult(73);
+		System.out.println();
+		System.out.print("136=");
+		getResult(136);
+		System.out.println();
+		System.out.print("255=");
+		getResult(255);
+		System.out.println();
+		System.out.print("1384=");
+		getResult(1384);
+		System.out.println();
+		System.out.print("16385=");
+		getResult(16385);
 	}
 
 	private static void getResult(int i) {
-//		int a = i;
-//		int twoPow = 0;
-//		System.out.print("2(");
-//		while (twoPow != i) {
-//			int twoPower = getTwoPower(a);
-//			twoPow += Math.pow(2,twoPower);
-//			System.out.println(twoPower);
-//			a=i-twoPow;
-//			if(a==i)
-//			{
-//				System.out.print(")");
-//			}
-//		}
-		
-		if(i>0)
-		{
-			int b=0;
-			while(i-b!=0)
-			{
-				System.out.print("2(");
-				int temp = getTwoPower(i);
-				if(temp>2)
-				{
+		if (i > 0) {
+			int b = i;
+			while (b != 0) {
+				int temp = 1;
+				System.out.print("2");
+				temp = getTwoPower(b);
+				if (temp > 2) {
+					System.out.print("(");
 					getResult(temp);
+					System.out.print(")");
+				} else if(temp!=1) {
+					System.out.print("(");
+					System.out.print(temp + ")");
 				}
-				else
-				{
-					System.out.print(temp+")");
-				}
-				b+=Math.pow(2,temp);
+				b -= Math.pow(2, temp);
+				if (b != 0)
+					System.out.print("+");
 			}
 		}
 	}
 
 	private static int getTwoPower(int a) {
 		int i = 0;
-		for (; Math.pow(2, i) < a; ++i) {
-			
+		for (; Math.pow(2, i) <= a; ++i) {
+
 		}
-		if(i-1>2)
-		{
-			System.out.print("2(");
-			System.out.print(getTwoPower(i-1));
-			System.out.print(")");
-		}
-		return i-1;
+		return i - 1;
 	}
 
 }
