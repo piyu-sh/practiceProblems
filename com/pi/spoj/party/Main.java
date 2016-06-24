b265e5e811825f616c03130880263cc6a477551c
pi <killerpgupta007@gmail.com>
2016-06-24 23:04:56 +0530

    spoj practice

--- com/pi/spoj/party/Main.java
+++ com/pi/spoj/party/Main.java
@@ -1,11 +1,12 @@
 package com.pi.spoj.party;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.TreeSet;
-
+//this approach failed
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
