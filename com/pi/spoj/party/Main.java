1fc8f1c6e20a4f29d5e4a3a75aa9ae52c11ea2c6
pi <killerpgupta007@gmail.com>
2016-06-29 22:58:11 +0530

    spoj practice

--- com/pi/spoj/party/Main.java
+++ com/pi/spoj/party/Main.java
@@ -17,7 +17,31 @@ public class Main {
 			String[] split = num.split(" ");
 			int money = Integer.parseInt(split[0].trim());
 			int party = Integer.parseInt(split[1].trim());
+			int fee[]=new int[party];
+			int fun[]=new int[party];
 			int dp[][]=new int[party+1][money+1];
+			for (int i = 0; i < party+1; i++) {
+				dp[i][0]=0;
+				String[] split2 = br.readLine().trim().split(" ");
+				fee[i]=Integer.parseInt(split2[0]);
+				fun[i]=Integer.parseInt(split2[1]);
+			}
+			for (int i = 0; i < money+1; i++) {
+				dp[0][i]=0;
+			}
+			
+			for (int i = 1; i <= party; i++) {
+				for (int j = 1; j <= money; j++) {
+					if(fee[i-1]<=j)
+					{
+				          dp[i][j]=Math.max(fun[i-1]+dp[i-1][j-fee[i-1]],dp[i-1][j]);
+					}else
+					{
+				          dp[i][j]=dp[i-1][j];
+					}
+						
+				}
+			}
 		}
 	}
 
