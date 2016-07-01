15015abfe869343a23da9bd237b4dbae87466b6e
pi <killerpgupta007@gmail.com>
2016-07-01 22:59:00 +0530

    spoj practice

--- com/pi/spoj/party/Main.java
+++ com/pi/spoj/party/Main.java
@@ -3,9 +3,7 @@ package com.pi.spoj.party;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.TreeSet;
+
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,32 +15,34 @@ public class Main {
 			String[] split = num.split(" ");
 			int money = Integer.parseInt(split[0].trim());
 			int party = Integer.parseInt(split[1].trim());
-			int fee[]=new int[party];
-			int fun[]=new int[party];
-			int dp[][]=new int[party+1][money+1];
-			for (int i = 0; i < party+1; i++) {
-				dp[i][0]=0;
+			int fee[] = new int[party];
+			int fun[] = new int[party];
+			int dp[][] = new int[party + 1][money + 1];
+			for (int i = 0; i < party; i++) {
 				String[] split2 = br.readLine().trim().split(" ");
-				fee[i]=Integer.parseInt(split2[0]);
-				fun[i]=Integer.parseInt(split2[1]);
+				fee[i] = Integer.parseInt(split2[0].trim());
+				fun[i] = Integer.parseInt(split2[1].trim());
 			}
-			for (int i = 0; i < money+1; i++) {
-				dp[0][i]=0;
-			}
-			
-			for (int i = 1; i <= party; i++) {
-				for (int j = 1; j <= money; j++) {
-					if(fee[i-1]<=j)
-					{
-				          dp[i][j]=Math.max(fun[i-1]+dp[i-1][j-fee[i-1]],dp[i-1][j]);
-					}else
-					{
-				          dp[i][j]=dp[i-1][j];
+			for (int i = 0; i < party + 1; i++) {
+				for (int j = 0; j < money + 1; j++) {
+					if (i == 0 || j == 0) {
+						dp[i][j] = 0;
 					}
-						
+					if (fee[i] <= j) {
+						dp[i][j] = Math.max(fun[i - 1]
+								+ dp[i - 1][j - fee[i - 1]], dp[i - 1][j]);
+					} else {
+						dp[i][j] = dp[i - 1][j];
+					}
+
 				}
 			}
-			 System.out.println(j+" "+ar[n][budget]);
+
+			int i = party, b = money;
+			while (dp[i][b] == dp[i][b - 1]) {
+				b--;
+			}
+			System.out.println(b + " " + dp[party][money]);
 		}
 	}
 
