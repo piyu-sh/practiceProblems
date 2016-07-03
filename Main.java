8b534ea7df219f8fedc4b860e132057502a68630
pi <killerpgupta007@gmail.com>
2016-07-03 21:53:36 +0530

    spoj practice

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) 
+					throws NumberFormatException, IOException {
+
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						System.in));
+				int N = Integer.parseInt(br.readLine().trim());
+				String split[]=br.readLine().split(" ");
+				int a[]=new int[N];
+				for (int i = 0; i < N; i++) {
+					a[i]=Integer.parseInt(split[i].trim());
+				}
+				// build segment tree here
+				int M=Integer.parseInt(br.readLine().trim());
+				for (int i = 0; i < M; i++) {
+					String splString[]=br.readLine().split(" ");
+					int x=Integer.parseInt(splString[0].trim());
+					int y=Integer.parseInt(splString[1].trim());
+					
+					//run query on segment tree
+				}
+			}
+
+	
+
+	int getSegmentTreeSize(int N) {
+		int size = 1;
+		for (; size < N; size <<= 1)
+			;
+		// a left-shift is a *=2, 2 left-shifts is a *=4 and so on. A
+		// right-shift is a /= 2 and so on
+		return size << 1;
+	}
+
+}
