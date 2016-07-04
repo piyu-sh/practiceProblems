7a9f6731667ae8f94f0a44a4e98f943dc65c6fb4
pi <killerpgupta007@gmail.com>
2016-07-04 23:01:23 +0530

    spoj practice

--- Main.java
+++ Main.java
@@ -4,29 +4,29 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) 
-					throws NumberFormatException, IOException {
+	public static void main(String[] args) throws NumberFormatException,
+			IOException {
 
-				BufferedReader br = new BufferedReader(new InputStreamReader(
-						System.in));
-				int N = Integer.parseInt(br.readLine().trim());
-				String split[]=br.readLine().split(" ");
-				int a[]=new int[N];
-				for (int i = 0; i < N; i++) {
-					a[i]=Integer.parseInt(split[i].trim());
-				}
-				// build segment tree here
-				int M=Integer.parseInt(br.readLine().trim());
-				for (int i = 0; i < M; i++) {
-					String splString[]=br.readLine().split(" ");
-					int x=Integer.parseInt(splString[0].trim());
-					int y=Integer.parseInt(splString[1].trim());
-					
-					//run query on segment tree
-				}
-			}
+		// USE fast input reader for ip
+		// USe stringbiulder to store op and print all at once at the end
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine().trim());
+		String split[] = br.readLine().split(" ");
+		int a[] = new int[N];
+		for (int i = 0; i < N; i++) {
+			a[i] = Integer.parseInt(split[i].trim());
+		}
+		// build segment tree here
+		int segTree[]=new int[2*N+1];
+		int M = Integer.parseInt(br.readLine().trim());
+		for (int i = 0; i < M; i++) {
+			String splString[] = br.readLine().split(" ");
+			int x = Integer.parseInt(splString[0].trim());
+			int y = Integer.parseInt(splString[1].trim());
 
-	
+			// run query on segment tree
+		}
+	}
 
 	int getSegmentTreeSize(int N) {
 		int size = 1;
